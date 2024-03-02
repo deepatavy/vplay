2fda2c50409885cbf089d8ca584acd81a8496b5b
Deep <75555496+deepatavy@users.noreply.github.com>
2024-03-02 19:52:17 +0530

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+# vplay
+
+A new Flutter demo project for interactive video player.
+
+## Flutter Version 3.16.8
+
---
+++ android/app/src/main/kotlin/com/example/vplay/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.vplay
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
