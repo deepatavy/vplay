8f19a33d1e10604567fb9f0eb149049424002b1f
Deep <75555496+deepatavy@users.noreply.github.com>
2024-03-02 21:28:04 +0530

    Added Assets

---
+++ lib/commons/asset_paths.dart
@@ -0,0 +1,4 @@
+   const String imgVidBw = 'assets/images/img_vid_bw.png';
+   const String imgVidColor = 'assets/images/img_vid_color.png';
+
+   const String sampleVideoPath = 'assets/videos/sample.mp4';
